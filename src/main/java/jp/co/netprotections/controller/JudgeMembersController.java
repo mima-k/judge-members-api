73095c93bcbe61fa6577ddf92dfe1f3650e5cebe
mima-k <mimakouhei@gmail,com>
2021-08-17 01:23:33 +0900

    Controllerの記述

--- src/main/java/jp/co/netprotections/controller/JudgeMembersController.java
+++ src/main/java/jp/co/netprotections/controller/JudgeMembersController.java
@@ -1,5 +1,24 @@
 package jp.co.netprotections.controller;
 
-public class JudgeMembersController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
+import jp.co.netprotections.service.JudgeMembersService;
+import jp.co.netprotections.dto.*;
+
+@RestController
+public class JudgeMembersController {
+	@Autowired
+	private JudgeMembersService judgeMembersService;
+	
+	@RequestMapping(value = "/api", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public MemberCandidatesListResponseDto execute(@RequestBody MemberCandidatesListRequestDto request) {
+		return judgeMembersService.judgedResult(request);
+	}
 }
